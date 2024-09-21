09af8da6c031ac347c943b28f339a4dfacf3d37f
Mosa Yaqoobi <mosayaqoobi@real-pro.local>
2024-09-20 21:22:55 -0600

    the abstract class Shape

---
+++ minion/app/src/main/java/com/example/minion/Shape.java
@@ -0,0 +1,22 @@
+package com.example.minion;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+    public Shape(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int getX(){
+        return this.x;
+    }
+    public int getY() {
+        return this.y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+}
