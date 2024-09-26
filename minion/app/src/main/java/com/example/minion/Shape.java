2e2910765b4bc6a662da9cc26cf8173c50444ebb
Preet Gami <109097825+preetgami@users.noreply.github.com>
2024-09-25 18:55:57 -0600

    Update Shape.java

--- minion/app/src/main/java/com/example/minion/Shape.java
+++ minion/app/src/main/java/com/example/minion/Shape.java
@@ -3,11 +3,9 @@ package com.example.minion;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color;
     public Shape(int x, int y, Stirng color){
         this.x = x;
         this.y = y;
-        this.color="yellow";
     }
     public int getX(){
         return this.x;
