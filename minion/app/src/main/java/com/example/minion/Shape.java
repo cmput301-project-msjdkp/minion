70b2e7169bef48315f6c34259c43023e57271b0d
preetgami <preetgami20@gmail.com>
2024-09-25 18:49:08 -0600

    color

--- minion/app/src/main/java/com/example/minion/Shape.java
+++ minion/app/src/main/java/com/example/minion/Shape.java
@@ -3,9 +3,11 @@ package com.example.minion;
 public abstract class Shape {
     private int x;
     private int y;
-    public Shape(int x, int y){
+    private String color;
+    public Shape(int x, int y,String color){
         this.x = x;
         this.y = y;
+        this.color='Blue'
     }
     public int getX(){
         return this.x;
