acd2a45a46b042b59f48cf1f092e7098ffff8f9a
Mosa <124646107+Mosayaqoobi@users.noreply.github.com>
2024-09-25 22:34:53 -0600

    Update Shape.java

--- minion/app/src/main/java/com/example/minion/Shape.java
+++ minion/app/src/main/java/com/example/minion/Shape.java
@@ -3,7 +3,7 @@ package com.example.minion;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "Pink";
+    private String color = "indigo";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
