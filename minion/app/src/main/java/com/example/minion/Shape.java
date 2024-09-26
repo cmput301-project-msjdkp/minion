4ce34d4a9b97ed5f8c37115588c5686fe52d5137
Shahzaib Ahmed <shahzaib@ualberta.ca>
2024-09-26 00:31:18 -0600

    changed to dark green

--- minion/app/src/main/java/com/example/minion/Shape.java
+++ minion/app/src/main/java/com/example/minion/Shape.java
@@ -3,7 +3,7 @@ package com.example.minion;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "red";
+    private String color = "dark green";
 
     public Shape(int x, int y){
         this.x = x;
